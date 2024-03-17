84b5a8cf699dc11d4742f96e8a53dabf0f9c8407
avrians <avriansyahb10@gmail.com>
2024-03-17 13:42:11 +0700

    [av][add][Memanggil Activity dari Fragment]

--- app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
+++ app/src/main/java/com/dicoding/myflexiblefragment/DetailCategoryFragment.kt
@@ -1,5 +1,6 @@
 package com.dicoding.myflexiblefragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -54,6 +55,11 @@ class DetailCategoryFragment : Fragment() {
 
             val fragemntManager = childFragmentManager
             optionDialogFragment.show(fragemntManager, OptionDialogFragment::class.java.simpleName)
+        }
+
+        btnProfile.setOnClickListener{
+            val intent = Intent(requireActivity(), ProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 
