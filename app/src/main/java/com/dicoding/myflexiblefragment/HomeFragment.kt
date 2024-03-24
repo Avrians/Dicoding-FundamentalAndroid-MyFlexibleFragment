c270f6b605f9370fbce526ab9dab5fabb02a2d89
avrians <avriansyahb10@gmail.com>
2024-03-24 10:37:18 +0700

    [av][update][ubah home fragment menggunakan Fragment KTX]

--- app/src/main/java/com/dicoding/myflexiblefragment/HomeFragment.kt
+++ app/src/main/java/com/dicoding/myflexiblefragment/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 
 class HomeFragment : Fragment(), View.OnClickListener {
     override fun onCreateView(
@@ -26,10 +27,17 @@ class HomeFragment : Fragment(), View.OnClickListener {
         if (v?.id == R.id.btn_category) {
             val categoryFragment = CategoryFragment()
             val fragmentManager = parentFragmentManager
-            fragmentManager.beginTransaction().apply {
-                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+//            tanpa menggunakan Fragment KTX
+//            fragmentManager.beginTransaction().apply {
+//                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+//                addToBackStack(null)
+//                commit()
+//            }
+
+//            menggunakan Fragment KTX
+            fragmentManager.commit {
                 addToBackStack(null)
-                commit()
+                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
             }
         }
     }
